d5ec5f00160773d800a25e4e9c463dde6517afa4
shevyakhov <shevyakhov89@gmail.com>
2022-08-19 15:03:38 +0700

    viemodel lazy delegate

--- app/src/main/java/com/example/autosnap/ui/translation/TranslationAndHistoryFragment.kt
+++ app/src/main/java/com/example/autosnap/ui/translation/TranslationAndHistoryFragment.kt
@@ -5,15 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.autosnap.databinding.FragmentTranslationAndHistoryBinding
+import com.example.autosnap.ui.translation.recycler_adapter.TextToTranslate
+import com.example.autosnap.ui.translation.recycler_adapter.TranslationAdapter
 
 class TranslationAndHistoryFragment : Fragment() {
 
     private var _binding: FragmentTranslationAndHistoryBinding? = null
     private val binding get() = _binding!!
+    private val adapter = TranslationAdapter()
 
-    private lateinit var viewModel: TranslationAndHistoryViewModel
+    private val viewModel by viewModels<TranslationAndHistoryViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +30,22 @@ class TranslationAndHistoryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this)[TranslationAndHistoryViewModel::class.java]
+        binding.searchBar.setOnClickListener {
+            binding.searchBar.onActionViewExpanded()
+        }
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager =
+            LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        adapter.passData(
+            arrayListOf(
+                TextToTranslate("some text\nsome text"),
+                TextToTranslate("some text"),
+                TextToTranslate("some text"),
+                TextToTranslate("some text"),
+                TextToTranslate("some text"),
+                TextToTranslate("blablabla\nsome text\nsome text")
+            )
+        )
     }
 
 
